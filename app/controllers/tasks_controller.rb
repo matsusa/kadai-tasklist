ee426a6c07d96d8bc73c1754e1803f1caf32325e
matsusa <llmatsusall@gmail.com>
2018-11-09 14:13:39 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in?
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.order('created_at DESC')
   end 
   
   def show
@@ -13,11 +14,11 @@ class TasksController < ApplicationController
   end 
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Task が正常に登録されました、頑張って'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task が登録されませんでした'
       render :new
@@ -47,16 +48,19 @@ class TasksController < ApplicationController
   
   private
   
+  def require_user_logged_in?
+    unless logged_in?
+    redirect_to login_url
+    end 
+  end 
+  
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
+    redirect_to root_url if !@task
   end 
   
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
-  end 
-  
-
-end
-
-
+  end
+end 
